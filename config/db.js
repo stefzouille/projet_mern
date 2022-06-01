fc235f19594a4c48e05859adcff973d2c22c7b7d
stefzouille <contact.lokcy@gmail.com>
2022-06-01 18:07:24 +0200

    add env bug ok

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: './config/.env' });
+require('dotenv').config({ path: '../.env' });
 
 // importer express
 const express = require('express');
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-require('dotenv').config({ path: './config/.env' });
+require('dotenv').config({ path: './.env' });
 require('./config/db');
 const app = express();
 
