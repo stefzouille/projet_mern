b246f2f0503057d52c8e0ebd5b45924e37cfbecb
stefzouille <contact.lokcy@gmail.com>
2022-06-02 13:33:46 +0200

    add apres JWT bug video tuto

--- config/db.js
+++ config/db.js
@@ -23,9 +23,9 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USER_PASS}@cluster0.9ylgaxd.mon
   .catch((err) => console.log('Connexion à MongoDB échouée !', err));
 
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//   next();
+// });
