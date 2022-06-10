d2bc230129110004f732e2f52a7fa873ae6da45c
stefzouille <contact.lokcy@gmail.com>
2022-06-10 17:12:04 +0200

    add follow/unf

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -83,7 +83,7 @@ module.exports.follow = async (req, res) => {
       //   else return res.status(400).jsos(err);
       // }
     )
-      .then((docs) => (console.log("ok")))
+      .then((docs) => res.status(201).send(docs))
       .catch((err) => res.status(500).send({ message: err }));
     // add to following list
     await UserModel.findByIdAndUpdate(
@@ -96,64 +96,11 @@ module.exports.follow = async (req, res) => {
       // }
     )
       .then((docs) => res.send(docs))
-      .catch((err) => res.status(500).send({ message: err }));
+      .catch((err) => res.status(500).send({ message: err }))
   } catch (err) {
     return res.status(500).json({ message: err });
   }
 };
-
-
-
-
-// module.exports.follow = async (req, res) => {
-//   if (
-//     !ObjectID.isValid(req.params.id) ||
-//     !ObjectID.isValid(req.body.idToFollow)
-//   )
-//     return res.status(400).send("ID unknown : " + req.params.id + console.log(req.params.id) + console.log(req.body.idToFollow));
-
-//   try {
-//     // add to the follower list
-//     await UserModel.findByIdAndUpdate(
-//       req.params.id,
-//       { $addToSet: { following: req.body.idToFollow } },
-//       { new: true, upsert: true },
-//       (err, docs) => {
-//         if (!err) console.log("ok");
-//         else return res.status(400).jsos(err);
-//       }
-//     );
-//     // add to following list
-//     await UserModel.findByIdAndUpdate(
-//       req.body.idToFollow,
-//       { $addToSet: { followers: req.params.id } },
-//       { new: true, upsert: true },
-//       (err, docs) => {
-//         // if (!err) res.status(201).json(docs);
-//         if (err) return res.status(400).jsos(err);
-//       }
-//     );
-//   } catch (err) {
-//     return res.status(500).json({ message: err });
-//   }
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports.unfollow = async (req, res) => {
   if (
@@ -167,22 +114,38 @@ module.exports.unfollow = async (req, res) => {
       req.params.id,
       { $pull: { following: req.body.idToUnfollow } },
       { new: true, upsert: true },
-      (err, docs) => {
-        if (!err) res.status(201).json(docs);
-        else return res.status(400).jsos(err);
-      }
-    );
+      // (err, docs) => {
+      //   if (!err) res.status(201).json(docs);
+      //   else return res.status(400).jsos(err);
+      // }
+    )
+      .then((docs) => res.status(201).send(docs))
+      .catch((err) => res.status(500).send({ message: err }));
     // remove to following list
     await UserModel.findByIdAndUpdate(
       req.body.idToUnfollow,
       { $pull: { followers: req.params.id } },
       { new: true, upsert: true },
-      (err, docs) => {
-        // if (!err) res.status(201).json(docs);
-        if (err) return res.status(400).jsos(err);
-      }
-    );
+      // (err, docs) => {
+      //   // if (!err) res.status(201).json(docs);
+      //   if (err) return res.status(400).jsos(err);
+      // }
+    )
+      // .then((docs) => res.send(docs))
+      .catch((err) => res.status(500).send({ message: err }))
   } catch (err) {
     return res.status(500).json({ message: err });
   }
 };
+
+
+
+
+
+
+
+
+
+
+
+
